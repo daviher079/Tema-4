d2832ee2c3564a859ab96846f55a0b4f922204ec
david <daviher079@gmail.com>
2021-12-10 15:52:37 +0000

    PDO Segunda Parte

--- codigo/pdo/conexion.php
+++ codigo/pdo/conexion.php
@@ -6,25 +6,92 @@ require_once("./segura/datosBD.php");
     {
         $con=new PDO($dsn, USER, PASS);
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Todo okey Jose Luis";
+        //echo "Todo okey Jose Luis";
 
         //$sql="insert into alumno values(350,'Federico',65);";
-        $sql="select * from alumno";
+        /*$sql="select * from alumno";
         $resultado=$con->query($sql);
 
-        while($row=$resultado->fetch())
+        while($row=$resultado->fetch(PDO::FETCH_ASSOC))
         {
             print_r($row);
+            echo"<br>";
+        }*/
+
+
+        //PREPARED STATEMENT
+
+        //FORMA 1
+
+        /*$preparada=$con->prepare("insert into alumno values (?,?,?)");
+        //crer el array antes o crearlo en el execute
+        $arrayParametros=array(123, 'Santiago', 35);
+        $preparada->execute($arrayParametros);*/
+
+        //FORMA 2
+
+        /*$preparada=$con->prepare("insert into alumno values (?,?,?)");
+        
+        $a=124;
+        $b='Pedro';
+        $c=45;
+        $preparada->bindParam(1, $a);
+        $preparada->bindParam(2, $b);
+        $preparada->bindParam(3, $c);
+        $preparada->execute();*/
+
+        //FORMA3 Con nombres en la consulta
+
+        /*$preparada=$con->prepare("select * from alumno where nombre like :nombre");
+
+        $nombrelike="%ia%";
+
+        $preparada->bindParam(":nombre", $nombrelike);
+
+        $preparada->execute();
+
+        $preparada->bindColumn(1,$id);
+        $preparada->bindColumn(2,$nombre);
+        $preparada->bindColumn(3,$edad);
+        echo $preparada->rowCount();
+
+        while($preparada->fetch())
+        {
+            echo "<br>".$id.":".$nombre.":".$edad;
+        }*/
+
+
+
+        //TRANSACCIONES
+
+        $preparada=$con->prepare("insert into alumno values (?,?,?)");
+        //crer el array antes o crearlo en el execute
+
+        $con->beginTransaction();
+
+        $arrayParametros=array(
+            array(120, 'Manuel', 15),
+            array(121, 'Miguel', 40),
+            array(122, 'Jose', 12),
+            array(123, 'Pepe', 22)
+        );
+
+        foreach ($arrayParametros as $value) {
+            
+            $preparada->execute($value);
         }
 
-
+        $con->commit();
 
 
         
     }catch(PDOException $ex)
     {
         //getMessage
+
+        $con->rollBack();
         echo "error ".$ex->getMessage();
+        echo "Nº ".$ex->getCode();
     }
     finally
     {
